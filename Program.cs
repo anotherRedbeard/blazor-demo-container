8f9411df4c7f11dacd19f86a9097c102106e31ef
anotherRedbeard <andrewredman@Redbeard-MBP.local>
2022-11-08 16:03:20 -0600

    test message

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+Console.WriteLine($"foo = {builder.Configuration["message"]}");
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
